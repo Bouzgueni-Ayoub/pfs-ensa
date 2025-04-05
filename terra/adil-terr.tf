36aff06a4fbd6f9c2e7b6717a0dd73d422af2504
Adil ARRAD <adil.arrad@gmail.com>
2025-04-05 16:32:15 +0000

    ygy

--- terra/adil-terr.tf
+++ terra/adil-terr.tf
@@ -112,6 +112,7 @@ resource "aws_instance" "adil-server" {
                 sudo apt update -y
                 sudo apt install apache2 -y
                 sudo systemctl start apache2
+                sudo systemctl enable apache2
                 sudo bash -c 'echo welcome to Ubuntu server made by Adil > /var/www/html/index.html' 
                 EOF
             tags = {
