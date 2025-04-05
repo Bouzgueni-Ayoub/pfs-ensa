21ba4d2790d2b0d5bf42fed44b27d1e3321ec7a1
Yahia ETTALIBI <yahia.ettalibi@edu.uca.ma>
2025-04-05 15:38:57 +0000

    hhfjfhsdf

--- terra/main.tf
+++ terra/main.tf
@@ -152,6 +152,7 @@ resource "aws_instance" "web_server_instance" {
           sudo apt update -y
           sudo apt install apache2 -y
           sudo systemctl start apache2
+          sudo systemctl enable apache2
           sudo bash -c 'echo your very first web server >/var/www/html/index.html'
           EOF
    tags = {
