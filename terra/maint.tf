682dad9113884210872e31a3015d389df1b54538
ayoub bouzgueni <bouzgueniayoub@gmail.com>
2025-04-07 20:06:36 +0000

    hh

--- terra/maint.tf
+++ terra/maint.tf
@@ -11,3 +11,21 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
+# VPC Module
+module "vpc" {
+  source = "./modules/vpc"
+  # Pass variables from the root to the module
+
+}
+
+# Security Group Module
+module "security_groups" {
+  source = "./modules/security_groups"
+
+}
+
+# EC2 Module
+module "ec2" {
+  source = "./modules/ec2"
+  
+}
