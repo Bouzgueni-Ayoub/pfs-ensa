d46ad75994482b5c9bed7bfba6d0e9b97bde652b
ayoub bouzgueni <bouzgueniayoub@gmail.com>
2025-04-13 13:10:26 +0000

    remote exec

--- terra/modules/ec2/ansible.tf
+++ terra/modules/ec2/ansible.tf
@@ -32,17 +32,19 @@ resource "aws_instance" "ansible_controller" {
 }
 
 resource "null_resource" "copy_ansible_files" {
-  depends_on = [aws_instance.ansible]
+  depends_on = [aws_instance.ansible_controller]
 
   connection {
     type        = "ssh"
-    host        = aws_instance.ansible.public_ip
+    host        = aws_instance.ansible_controller.public_ip
     user        = "ubuntu"
     private_key = file("${path.module}/../../main-key.pem")
   }
 
-  provisioner "file" {
-    source      = "${path.module}/ansible"
-    destination = "/tmp/ansible"
-}
+  provisioner "remote-exec" {
+    inline = [
+      "mkdir -p /tmp/ansible",
+      "rsync -avz -e 'ssh -o StrictHostKeyChecking=no' ${path.module}/ansible/ ubuntu@${aws_instance.ansible_controller.public_ip}:/tmp/ansible"
+    ]
+  }
 }
