5c43f517fa53d5192cb171c5da823e1892526bbd
Yahia ETTALIBI <yahia.ettalibi@edu.uca.ma>
2025-04-21 11:24:52 +0000

    variable.yml changes of wireguard

--- terra/modules/ec2/ansible-userdata.sh
+++ terra/modules/ec2/ansible-userdata.sh
@@ -79,3 +79,15 @@ chown -R ubuntu:ubuntu /home/ubuntu
 
 # Optional: make scripts or playbooks executable
 chmod +x /opt/ansible/*.yml
+
+# Replace with the name tag of your WireGuard EC2 instance
+INSTANCE_NAME="wireguard-server" 
+
+# Get the public IP of the EC2 instance by its Name tag
+WIREGUARD_IP=$(aws ec2 describe-instances \
+  --filters "Name=tag:Name,Values=$INSTANCE_NAME" \
+  --query "Reservations[0].Instances[0].PublicIpAddress" \
+  --output text)
+
+# Write the IP to variables.yml
+echo "wireguard_ip: $WIREGUARD_IP" > /ansible/variables.yml
